ad46ca26b12a4e6a4a55bc855b8661de5f3455ea
Yu Sakakibara <ict25095@stu.cbc.ac.jp>
2026-09-11 21:05:25 +0900

    not yet

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -33,16 +33,36 @@ class DataProcessor(ABC):
                 return False
 
 
+        def ingest(self, data: int | float | list[int | float]) -> None:
+            try:
+                if not self.validata(data):
+                    raise TypeError("Got exception: Improper numeric data")
+                if isinstance(data, list):
+                    for content in data:
+                        self._storage.append((self._rank, str(content)))
+                        self._rank += 1
+                else:
+                    self._storage.append((self._rank, str(data)))
+                    self._rank += 1
+            except TypeError as e:
+                print(e)
 
-def test_numeric() -> None:
-    print("Testing Numeric Processor...")
-    numeric: NumericProcessor = NumericProcessor()
+
+# def test_numeric() -> None:
+#     print("Testing Numeric Processor...")
+#     numeric: NumericProcessor = NumericProcessor()
 
 def main() -> None:
     print("=== Code Nexus - Data Processor ===")
-    test_numeric()
-    test_text()
-    test_log()
+    print()
+    print("Testing Numeric Processor...")
+    test1: list[int | str] = [42, "Hello"]
+    num = NumericProcessor()
+    for example in test1:
+        print(
+            f"Trying to validate input '{example}':"
+            f" {num.validate(example)}"
+            )
 
 if __name__ == "__main__":
     main()
