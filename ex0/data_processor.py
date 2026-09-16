ee6b982a40bc31cdf00a20140a615339d6a95a76
Yu Sakakibara <yusakaki@c3r4s2.42tokyo.jp>
2026-09-16 18:41:50 +0900

    confuse output

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -20,89 +20,92 @@ class DataProcessor(ABC):
     def output(self) -> tuple[int, str]:
         return self._storage.pop(0)
 
-    class NumericProcessor(DataProcessor):
-        def validate(self, data: Any) -> bool:
-            if isinstance(data, (int, float)):
-                return True
-            elif isinstance(data, list):
-                for content in data:
-                    if not isinstance(content, (int, float)):
-                        return False
-                return True
-            else:
-                return False
 
-        def ingest(self, data: int | float | list[int | float]) -> None:
-            try:
-                if not self.validata(data):
-                    raise TypeError("Got exception: Improper numeric data")
-                if isinstance(data, list):
-                    for content in data:
-                        self._storage.append((self._rank, str(content)))
-                        self._rank += 1
-                else:
-                    self._storage.append((self._rank, str(data)))
-                    self._rank += 1
-            except TypeError as e:
-                print(e)
-
-    class TextProcessor(DataProcessor):
-        def validate(self, data: Any) -> bool:
-            if isinstance(data, str):
-                return True
-            elif isinstance(data, list):
-                for content in data:
-                    if not isinstance(content, str):
-                        return False
-                    return True
-                else:
+class NumericProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (int, float)):
+            return True
+        elif isinstance(data, list):
+            for content in data:
+                if not isinstance(content, (int, float)):
                     return False
+            return True
+        else:
+            return False
 
-        def ingest(self, data: str | list[str]) -> None:
-            try:
-                if not self.validate(data):
-                    raise TypeError("Got exception: Improper text data")
-                if isinstance(data, list):
-                    for content in data:
-                        self._storage.append((self._rank, str(content)))
-                        self._rank += 1
-                else:
-                    self._storage.append(self._rank, str(data))
-                    self._rank += 1
-            except TypeError as e:
-                print(e)
-
-    class LogProcessor(DataProcessor):
-        def validate(self, data:Any) -> Bool:
-            if isinstance(data, dict):
-                return True
-            elif isinstance(data, list):
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        try:
+            if not self.validate(data):
+                raise TypeError("Got exception: Improper numeric data")
+            if isinstance(data, list):
                 for content in data:
-                    if not isinstance(content, dict):
-                        return False
-                    return True
+                    self._storage.append((self._rank, str(content)))
+                    self._rank += 1
+            else:
+                self._storage.append((self._rank, str(data)))
+                self._rank += 1
+        except TypeError as e:
+            print(e)
+
+
+class TextProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list):
+            for content in data:
+                if not isinstance(content, str):
+                    return False
+                return True
             else:
                 return False
 
-        def ingest(self, data: dict[str, str] |list[dict[str, str]]) -> None:
-            try:
-                if not self.validate(data):
-                    raise TypeError("Got exception: Improper log data")
-                if isinstance(data, list):
-                    for content in data:
-                        self._storage.append(
-                            self._rank,
-                            str(f"{content['log_level']}:"
-                                f"{content['log_message']}")
-                            )
-                        self._rank += 1
-                else:
-                    self._storage.append((
-                        self._rank,
-                        str(f"{data['log_level']}: {data['log_message']}")))
+    def ingest(self, data: str | list[str]) -> None:
+        try:
+            if not self.validate(data):
+                raise TypeError("Got exception: Improper text data")
+            if isinstance(data, list):
+                for content in data:
+                    self._storage.append((self._rank, str(content)))
                     self._rank += 1
-            except TypeError as e:
-                print(e)
+            else:
+                self._storage.append(self._rank, str(data))
+                self._rank += 1
+        except TypeError as e:
+            print(e)
+
+
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            return True
+        elif isinstance(data, list):
+            for content in data:
+                if not isinstance(content, dict):
+                    return False
+                return True
+        else:
+            return False
+
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
+        try:
+            if not self.validate(data):
+                raise TypeError("Got exception: Improper log data")
+            if isinstance(data, list):
+                for content in data:
+                    self._storage.append(
+                        self._rank,
+                        str(f"{content['log_level']}:"
+                            f"{content['log_message']}")
+                    )
+                    self._rank += 1
+            else:
+                self._storage.append((
+                    self._rank,
+                    str(f"{data['log_level']}: {data['log_message']}")))
+                self._rank += 1
+        except TypeError as e:
+            print(e)
 
 
 def main() -> None:
@@ -115,15 +118,22 @@ def main() -> None:
         print(
             f"Trying to validate input '{example}':"
             f" {num.validate(example)}"
-            )
+        )
     test2: str = "foo"
     print(
         "Test invalid ingestion of string "
         f"'{test2}' without prior validation:"
-        )
+    )
     num.ingest(test2)
-    
 
+    test3: list[list | float] = [1, 2, 3, 4, 5]
+    print(f"processing data: {test3}")
+    num.ingest(test3)
+    number: int = 3
+    print(f"Extracting {number} value...")
+    for i in range(0, number):
+        key, value = num.output()
+        print(f"Numeric value {i}: {number}")
 
 
 if __name__ == "__main__":
